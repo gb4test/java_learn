2b585fb1abc0dd0be993d1ac054f746ae9b69375
gb4test <52651267+gb4test@users.noreply.github.com>
2021-02-07 12:49:25 +0100

    Создание общего базового класса для классов-помощников

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -3,14 +3,11 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class NavigationHelper {
-    private WebDriver wd;
+public class NavigationHelper extends HelperBase {
 
-    public NavigationHelper(WebDriver wd) {
-        this.wd = wd;
-    }
+    public NavigationHelper(WebDriver wd) { super(wd); }
 
     public void goToGroupPage() {
-        wd.findElement(By.linkText("groups")).click();
+        click(By.linkText("groups"));
     }
 }
