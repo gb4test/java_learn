b21bb8dad6f8437620fbbd9c8152da49646fc061
gb4test <52651267+gb4test@users.noreply.github.com>
2021-02-07 19:20:36 +0100

    Трансформированные тесты групп и контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests2.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests2.java
@@ -8,7 +8,8 @@ public class ContactCreationTests2 extends TestBase {
   @Test
   public void testContactCreation() {
     app.getContactHelper().addNewContact();
-    app.getContactHelper().fillContactForm(new ContactData("ret", "rew", "trry.jfhj, khaf 54",
+    app.getContactHelper().fillContactForm(new ContactData("ret", "rew",
+            "trry.jfhj, khaf 54",
             "46546", "4533", "4664", "4",
             "dfsdf@sca.adf", "dgagddg.sdg@dsf.fgr", "afdf@dfda.sdv"));
     app.getContactHelper().submitContactCreation();
