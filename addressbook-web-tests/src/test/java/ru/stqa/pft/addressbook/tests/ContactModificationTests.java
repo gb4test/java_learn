3aeb62172316822efe6a7f3a6c2927c47f4f3760
gb4test <52651267+gb4test@users.noreply.github.com>
2021-02-07 21:32:50 +0100

    Тест для редактирования контакта

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactModificationTests extends TestBase {
+
+    @Test
+    public void testContactModification() {
+        app.getNavigationHelper().goToHome();
+        app.getContactHelper().initContactModification();
+        app.getContactHelper().fillContactForm(new ContactData("new", "t",
+                "Florida, 5 avenu, 954-56",
+                "1", "11", "111", "1",
+                "111@sca.adf", "111.sdg@dsf.fgr", "11@dfda.sdv"));
+        app.getContactHelper().updateContactModification();
+        app.getNavigationHelper().goToHomePage();
+    }
+}
