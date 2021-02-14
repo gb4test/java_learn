9ab0427b8d4b113dce610f166fb8b779c2c22199
gb4test <52651267+gb4test@users.noreply.github.com>
2021-02-14 15:49:35 +0100

    Added check for availability contact for deletion.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeleteTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeleteTest.java
@@ -1,12 +1,19 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
 
 public class ContactDeleteTest extends TestBase {
 
     @Test
     public void testContactDelete() {
         app.getNavigationHelper().goToHome();
+        if (! app.getContactHelper().isThereAContact()) {
+            app.getContactHelper().createContact(new ContactData("ret", " rew",
+                    "trry.jfhj, khaf 54",
+                    "46546", "4533", "4664", "4",
+                    "dfsdf@sca.adf", "dgagddg.sdg@dsf.fgr", "afdf@dfda.sdv", "test"), true);
+        }
         app.getContactHelper().selectContact();
         app.getContactHelper().deleteContact();
         app.getContactHelper().isAlertAccept();
