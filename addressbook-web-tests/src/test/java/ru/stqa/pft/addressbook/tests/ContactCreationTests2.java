ba1fddcec7965f273d87826e6072ad38381ea765
gb4test <52651267+gb4test@users.noreply.github.com>
2021-02-23 17:18:11 +0100

    Changing variable to list in Contact Creation

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests2.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests2.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.List;
+
 public class ContactCreationTests2 extends TestBase {
 
   @Test
@@ -11,11 +13,11 @@ public class ContactCreationTests2 extends TestBase {
     app.getNavigationHelper().goToGroupPage();
     app.getGroupHelper().checkGroup();
     app.getNavigationHelper().goToHome();
-    int before = app.getContactHelper().getContactCount();
+    List<ContactData> before = app.getContactHelper().getContactList();
     app.getContactHelper().createContact (new ContactData("Lui", " rew",
             "N.n, n  54", "46546", "4533", "4664", "4",
             "dfsdf@sca.adf", "dgagddg.sdg@dsf.fgr", "afdf@dfda.sdv", "test"), true);
-    int after = app.getContactHelper().getContactCount();
-    Assert.assertEquals(after, before + 1);
+    List<ContactData> after = app.getContactHelper().getContactList();
+    Assert.assertEquals(after.size(), before.size() + 1);
   }
 }
