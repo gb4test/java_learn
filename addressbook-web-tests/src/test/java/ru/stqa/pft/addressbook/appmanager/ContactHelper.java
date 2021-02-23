1eafc45af68e651d4278b80ab482f25ff1816218
gb4test <52651267+gb4test@users.noreply.github.com>
2021-02-23 17:15:55 +0100

    Forming contact list in Contact Helper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -2,9 +2,13 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContactHelper extends HelperBase {
 
@@ -40,13 +44,15 @@ public class ContactHelper extends HelperBase {
 
     }
 
-    public void selectContact() { click(By.name("selected[]")); }
+    public void selectContact(int index) {
+        wd.findElements(By.name("selected[]")).get(index).click();
+    }
 
     public void deleteContact() { click(By.xpath("//input[@value='Delete']")); }
 
     public void isAlertAccept() { wd.switchTo().alert().accept(); }
 
-    public void initContactModification() { click(By.xpath("//img[@alt='Edit']")); }
+    public void initContactModification() { click(By.xpath("(//img[@alt='Edit'])")); }
 
     public void updateContactModification() { click(By.name( "update"));}
 
@@ -67,13 +73,18 @@ public class ContactHelper extends HelperBase {
         return wd.findElements(By.name("selected[]")).size();
     }
 
-    public void checkContact() {
-        if (! isThereAContact()) {
-            createContact(new ContactData("ret", " rew",
-                    "trry.jfhj, khaf 54",
-                    "46546", "4533", "4664", "4",
-                    "dfsdf@sca.adf", "dgagddg.sdg@dsf.fgr", "afdf@dfda.sdv", "test"), true);
+    public List<ContactData> getContactList() {
+        List<ContactData> contacts = new ArrayList<ContactData>();
+        List<WebElement> elements = wd.findElements(By.cssSelector("tr"));
+        for (WebElement element : elements) {
+            String firstName = element.getText();
+            String lastName = element.getText();
+
+            ContactData contact = new ContactData(firstName, lastName, null, null, null, null, null,
+                    null, null, null, null);
+            contacts.add(contact);
         }
+        return contacts;
     }
 
 }
