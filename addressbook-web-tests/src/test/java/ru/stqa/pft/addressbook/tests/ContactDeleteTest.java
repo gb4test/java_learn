babe09dc3ab86e3ce356b3a0911c3cfc815e1b66
gb4test <52651267+gb4test@users.noreply.github.com>
2021-02-23 17:18:39 +0100

    Changing variable to list in Contact Deletion

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeleteTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeleteTest.java
@@ -2,22 +2,30 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.util.List;
 
 public class ContactDeleteTest extends TestBase {
 
     @Test
     public void testContactDelete() {
-        app.getNavigationHelper().goToGroupPage();
-        app.getGroupHelper().checkGroup();
+
+        if (! app.getContactHelper().isThereAContact()) {
+            app.getNavigationHelper().goToGroupPage();
+            app.getGroupHelper().checkGroup();
+            app.getContactHelper().createContact(new ContactData("Lui", " rew",
+                    "N.n, n  54", "46546", "4533", "4664", "4",
+                    "dfsdf@sca.adf", "dgagddg.sdg@dsf.fgr", "afdf@dfda.sdv", "test"), true);
+        }
         app.getNavigationHelper().goToHome();
-        app.getContactHelper().checkContact();
-        app.getNavigationHelper().goToHome();
-        int before = app.getContactHelper().getContactCount();
-        app.getContactHelper().selectContact();
+        List<ContactData> before = app.getContactHelper().getContactList();
+        System.out.println("before.size(): " + before.size());
+        app.getContactHelper().selectContact(before.size() - 2);
         app.getContactHelper().deleteContact();
         app.getContactHelper().isAlertAccept();
         app.getNavigationHelper().goToHome();
-        int after = app.getContactHelper().getContactCount();
-        Assert.assertEquals(after, before - 1);
+        List<ContactData> after = app.getContactHelper().getContactList();
+        Assert.assertEquals(after.size(), before.size() - 1);
     }
 }
