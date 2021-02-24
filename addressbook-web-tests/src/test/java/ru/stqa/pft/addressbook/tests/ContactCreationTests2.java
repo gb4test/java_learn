821a5ed5c3b52b6ed08fe576db4722da9e26936c
gb4test <52651267+gb4test@users.noreply.github.com>
2021-02-24 16:43:46 +0100

    Searching max id

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests2.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests2.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class ContactCreationTests2 extends TestBase {
@@ -14,10 +15,22 @@ public class ContactCreationTests2 extends TestBase {
     app.getGroupHelper().checkGroup();
     app.getNavigationHelper().goToHome();
     List<ContactData> before = app.getContactHelper().getContactList();
-    app.getContactHelper().createContact (new ContactData("Lui", " rew",
+    ContactData contact = new ContactData("CreatedNow", "rew",
             "N.n, n  54", "46546", "4533", "4664", "4",
-            "dfsdf@sca.adf", "dgagddg.sdg@dsf.fgr", "afdf@dfda.sdv", "test"), true);
+            "dfsdf@sca.adf", "dgagddg.sdg@dsf.fgr", "afdf@dfda.sdv", "test");
+    app.getContactHelper().createContact (contact, true);
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1);
+
+
+    int max = 0;
+    for (ContactData g : after) {
+      if (g.getId() > max) {
+        max = g.getId();
+      }
+    }
+    contact.setId(max);
+    before.add(contact);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 }
