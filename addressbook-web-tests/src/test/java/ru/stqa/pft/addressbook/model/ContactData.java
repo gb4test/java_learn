2f7f48eecd02a59a191aefe157229606236a3a25
gb4test <52651267+gb4test@users.noreply.github.com>
2021-02-24 17:49:29 +0100

    Deleted id from comparison in Contact Data

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -36,7 +36,7 @@ public class ContactData {
     public ContactData( String firstname, String lastname, String address,
                        String home_phone, String mobile_phone, String work_phone, String fax,
                        String email, String email2, String email3, String group) {
-        this.id = 0;
+        this.id = Integer.MAX_VALUE;
         this.firstname = firstname;
         this.lastname = lastname;
         this.address = address;
@@ -110,12 +110,12 @@ public class ContactData {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         ContactData that = (ContactData) o;
-        return id == that.id && Objects.equals(firstname, that.firstname) && Objects.equals(lastname, that.lastname);
+        return Objects.equals(firstname, that.firstname) && Objects.equals(lastname, that.lastname);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, firstname, lastname);
+        return Objects.hash(firstname, lastname);
     }
 
     public void setId(int id) {
