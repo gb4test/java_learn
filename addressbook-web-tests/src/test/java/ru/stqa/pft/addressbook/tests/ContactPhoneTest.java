acac5979e5165877f78c1c371a5ad41e83776de9
gb4test <52651267+gb4test@users.noreply.github.com>
2021-03-01 00:38:36 +0100

    Added beforeMethod in ContactPhoneTest

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTest.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -11,9 +12,22 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactPhoneTest extends TestBase {
 
+    @BeforeMethod
+    public void ensurePreconditions() {
+        if (app.contact().all().size() == 0) {
+            app.goTo().groupPage();
+            app.group().check();
+            app.contact().create(
+                    new ContactData().withFirstname("CreatedNow").withLastname("rew").withAddress("N.n, n54")
+                            .withHome_phone("255-25-25").withMobile_phone("45 33").withWork_phone("+4(664) ")
+                            .withFax("4").withEmail("dfsdf@sca.adf").withEmail2("dgagddg.sdg@dsf.fgr")
+                            .withEmail3("afdf@dfda.sdv").withGroup("test"), true);
+        }
+        app.goTo().home();
+    }
+
     @Test
     public void testContactPhones() {
-        app.goTo().home();
         ContactData contact = app.contact().all().iterator().next();
         ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
 
