555010e4eaf6331f31730d021466e83a6c21f638
gb4test <52651267+gb4test@users.noreply.github.com>
2021-03-08 12:59:14 +0100

    loading information about test bench from the configuration file

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -5,18 +5,20 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
+import java.io.IOException;
+
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected static final ApplicationManager app
+      = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
-    @BeforeSuite
-    public void setUp() {
-        app.init();
-    }
+  @BeforeSuite
+  public void setUp() throws IOException {
+    app.init();
+  }
 
-    @AfterSuite
-    public void tearDown() {
-        app.stop();
-    }
-
+  @AfterSuite
+  public void tearDown() {
+    app.stop();
+  }
 }
