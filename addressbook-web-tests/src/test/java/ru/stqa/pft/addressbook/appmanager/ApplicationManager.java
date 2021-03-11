8e93e87288992dec1ec403f93eda2e322a982a44
gb4test <52651267+gb4test@users.noreply.github.com>
2021-03-11 17:57:01 +0100

    Reformat code

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -6,59 +6,60 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
-public class  ApplicationManager {
-    private Properties properties;
-    protected WebDriver wd;
+public class ApplicationManager {
+  private Properties properties;
+  protected WebDriver wd;
 
-    private SessionHelper sessionHelper;
-    private NavigationHelper navigationHelper;
-    private GroupHelper groupHelper;
-    private ContactHelper contactHelper;
-    private String browser;
+  private SessionHelper sessionHelper;
+  private NavigationHelper navigationHelper;
+  private GroupHelper groupHelper;
+  private ContactHelper contactHelper;
+  private String browser;
 
-    public ApplicationManager(String browser){
-        this.browser = browser;
-        properties = new Properties();
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+    properties = new Properties();
+  }
+
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(String.format("src/test/resources/%s.properties", target)));
+
+    if (browser.equals(BrowserType.FIREFOX)) {
+      wd = new FirefoxDriver();
+    } else if (browser.equals(BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    } else if (browser.equals(BrowserType.IE)) {
+      wd = new InternetExplorerDriver();
     }
+    wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    wd.get(properties.getProperty("web.baseUrl"));
+    groupHelper = new GroupHelper(wd);
+    navigationHelper = new NavigationHelper(wd);
+    contactHelper = new ContactHelper(wd);
+    sessionHelper = new SessionHelper(wd);
+    sessionHelper.login(properties.getProperty("web.adminLogin"),
+        properties.getProperty("web.adminPassword"));
+  }
 
-    public void init()  throws IOException {
-        String target = System.getProperty("target", "local");
-        properties.load(new FileReader(String.format("src/test/resources/%s.properties", target)));
+  public void stop() {
+    wd.quit();
+  }
 
-        if (browser.equals(BrowserType.FIREFOX)) {
-            wd = new FirefoxDriver();
-        } else if (browser.equals(BrowserType.CHROME)) {
-            wd = new ChromeDriver();
-        } else if (browser.equals(BrowserType.IE)) {
-            wd = new InternetExplorerDriver();
-        }
-        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        wd.get(properties.getProperty("web.baseUrl"));
-        groupHelper = new GroupHelper(wd);
-        navigationHelper = new NavigationHelper(wd);
-        contactHelper = new ContactHelper(wd);
-        sessionHelper = new SessionHelper(wd);
-        sessionHelper.login(properties.getProperty("web.adminLogin"),
-            properties.getProperty("web.adminPassword"));
-    }
+  public GroupHelper group() {
+    return groupHelper;
+  }
 
-    public void stop() {
-        wd.quit();
-    }
+  public ContactHelper contact() {
+    return contactHelper;
+  }
 
-    public GroupHelper group() {
-        return groupHelper;
-    }
-
-    public ContactHelper contact() { return contactHelper; }
-
-    public NavigationHelper goTo() {
-        return navigationHelper;
-    }
+  public NavigationHelper goTo() {
+    return navigationHelper;
+  }
 }
