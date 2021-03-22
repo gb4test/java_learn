2b11d01d5cca84a393bd54c7b269a7eeeaeac68f
gb4test <52651267+gb4test@users.noreply.github.com>
2021-03-22 22:09:12 +0100

    Interaction with the server via the HTTP protocol

--- mantis-test/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
+++ mantis-test/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
@@ -34,8 +34,10 @@ public class HttpSession {
     post.setEntity(new UrlEncodedFormEntity(params));
     CloseableHttpResponse response = httpclient.execute(post);
     String body = geTextFrom(response);
-    return body.contains(String.format("<span class=\"italic\">%s</span>", username));
+    return body.contains(String.format("<span class=\"user-info\">%s</span>", username));
   }
+
+  // <span class="user-info">%s</span>
 
   private String geTextFrom(CloseableHttpResponse response) throws IOException {
     try {
@@ -49,6 +51,6 @@ public class HttpSession {
     HttpGet get = new HttpGet(app.getProperty("web.baseUrl") + "/index.php");
     CloseableHttpResponse response = httpclient.execute(get);
     String body = geTextFrom(response);
-    return body.contains(String.format("<span class=\"italic\">%s</span>", username));
+    return body.contains(String.format("<span class=\"user-info\">%s</span>", username));
   }
 }
