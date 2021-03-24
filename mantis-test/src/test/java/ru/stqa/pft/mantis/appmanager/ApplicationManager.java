3056e6eb301010ab86ab2db83265728d092d88f1
gb4test <52651267+gb4test@users.noreply.github.com>
2021-03-24 22:40:54 +0100

    Receiving mail to mail server built in tests

--- mantis-test/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-test/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -1,24 +1,26 @@
 package ru.stqa.pft.mantis.appmanager;
 
+import org.apache.http.impl.client .HttpClients;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-  private Properties properties;
+  private final Properties properties;
   private WebDriver wd;
 
   private String browser;
   private RegistrationHelper registrationHelper;
   private FtpHelper ftp;
-
+  private MailHelper mailHelper;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -27,7 +29,7 @@ public class ApplicationManager {
 
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
-    properties.load(new FileReader(String.format("src/test/resources/%s.properties", target)));
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
   }
 
   public void stop() {
@@ -46,7 +48,7 @@ public class ApplicationManager {
 
   public RegistrationHelper registration() {
     if (registrationHelper == null) {
-    registrationHelper = new RegistrationHelper(this);
+      registrationHelper = new RegistrationHelper(this);
     }
     return registrationHelper;
   }
@@ -67,9 +69,16 @@ public class ApplicationManager {
       } else if (browser.equals(BrowserType.IE)) {
         wd = new InternetExplorerDriver();
       }
-      wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+      wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
       wd.get(properties.getProperty("web.baseUrl"));
     }
     return wd;
+  }
+
+  public MailHelper mail() {
+    if (mailHelper == null) {
+      mailHelper = new MailHelper(this);
+    }
+    return mailHelper;
   }
 }
