478ed82cc1e8ca3a2b3d321ffed776308cf51efb
gb4test <52651267+gb4test@users.noreply.github.com>
2021-03-31 00:47:05 +0200

    assertequals total (not set)

--- rest-sample/src/test/java/ru/stqa/pft/rest/RestAssuresTests.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/RestAssuresTests.java
@@ -1,12 +1,14 @@
-package ru.stqa.pft.rest.rest;
+package ru.stqa.pft.rest;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
 import com.jayway.restassured.RestAssured;
+import org.apache.http.client.fluent.Request;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
 
 import java.io.IOException;
 import java.util.Set;
@@ -17,17 +19,17 @@ public class RestAssuresTests {
 
   @BeforeClass
   public void init() {
-    RestAssured.authentication = RestAssured.basic("679794dc940014a6013d4a290a73c9ad", "");
+    RestAssured.authentication = RestAssured.basic("3e705c82933119827aa93e56b6754242", "");
   }
+
 
   @Test
   public void testCreateIssue() throws IOException {
-    Set<Issue> oldIssues = getIssues();
-    Issue newIssue = new Issue().withSubject("Test issue").withDescription("New test issue");
-    int issueId = createIssue(newIssue);
-    Set<Issue> newIssues = getIssues();
-    oldIssues.add(newIssue.withId(issueId));
-    assertEquals(newIssues, oldIssues);
+    int oldIssues = getTotal();
+    Issue newIssue = new Issue().withSubject("Definite Test issue").withDescription("New test issue");
+    createIssue(newIssue);
+    int newIssues = getTotal();
+    assertEquals(newIssues, oldIssues + 1);
   }
 
   private Set<Issue> getIssues() throws IOException {
@@ -35,6 +37,14 @@ public class RestAssuresTests {
     JsonElement parsed = new JsonParser().parse(json);
     JsonElement issues = parsed.getAsJsonObject().get("issues");
     return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
+  }
+
+  private int getTotal() throws IOException {
+    String json = RestAssured.get("http://demo.bugify.com/api/issues.json").asString();
+    JsonElement parsed = new JsonParser().parse(json);
+    JsonElement issues = parsed.getAsJsonObject().get("issues");
+    int total = parsed.getAsJsonObject().get("total").getAsInt();
+    return total;
   }
 
   private int createIssue(Issue newIssue) throws IOException {
--- rest-sample/src/test/java/ru/stqa/pft/rest/RestTests.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/RestTests.java
@@ -18,12 +18,11 @@ public class RestTests {
 
   @Test
   public void testCreateIssue() throws IOException {
-    Set<Issue> oldIssues = getIssues();
+    int oldIssues = getTotal();
     Issue newIssue = new Issue().withSubject("Definite Test issue").withDescription("New test issue");
-    int issueId = createIssue(newIssue);
-    Set<Issue> newIssues = getIssues();
-    oldIssues.add(newIssue.withId(issueId));
-    assertEquals(newIssues, oldIssues);
+    createIssue(newIssue);
+    int newIssues = getTotal();
+    assertEquals(newIssues, oldIssues + 1);
   }
 
   private Set<Issue> getIssues() throws IOException {
@@ -34,8 +33,17 @@ public class RestTests {
     return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
   }
 
+  private int getTotal() throws IOException {
+    String json = getExecutor().execute(Request.Get("http://demo.bugify.com/api/issues.json"))
+        .returnContent().asString();
+    JsonElement parsed = new JsonParser().parse(json);
+    JsonElement issues = parsed.getAsJsonObject().get("issues");
+    int total = parsed.getAsJsonObject().get("total").getAsInt();
+    return total;
+  }
+
   private Executor getExecutor() {
-    return Executor.newInstance().auth("99454f7f4297c9ee1578cfd6cfb2e89b", "");
+    return Executor.newInstance().auth("3e705c82933119827aa93e56b6754242", "");
   }
 
   private int createIssue(Issue newIssue) throws IOException {
