46d4b4f80b69d3bacd40fe16f0804b8a80e898ff
gb4test <52651267+gb4test@users.noreply.github.com>
2021-04-02 20:55:57 +0200

    edit class rectangle

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -10,7 +10,7 @@ public class MyFirstProgram {
        Square s = new Square(5);
        System.out.println("Square area with side " + s.l + " = " + s.area());
 
-       Rectangle r = new Rectangle(4, 6);
+       Rectangles r = new Rectangles(4, 6);
        System.out.println("Area of a rectangle with sides " + r.a + " and " + r.b + " = " + r.area());
    }
 
